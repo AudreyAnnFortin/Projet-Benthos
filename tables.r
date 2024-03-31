dd432eb1a52d3b713f0d8576b42501170a7b9599
YourName <your@mail.com>
2024-03-31 17:18:03 -0400

    Ajout de la transformation en CSV

--- tables.r
+++ tables.r
@@ -11,6 +11,8 @@ for (file_name in names(data_list)) {
   # Add these to the observations data frame
   observations <- rbind(observations, selected_columns)
 }
+#put the data frame into a CSV file to be read into the data base
+write.csv(observations, "observations.csv",row.names = F)
 
 
 # Create a data frame to insert the different variables needed for the site's data frame
@@ -22,7 +24,8 @@ for (file_name in names(data_list)) {
   # Add these to the site data frame
   site <- rbind(site, selected_columns)
 }
-
+#put the data frame into a CSV file to be read into the data base
+write.csv(site, "sites.csv",row.names =F)
 
 # Create a data frame to insert the different variables needed for the date_observed's data frame
 date_observed<- data.frame(date=as.Date(character()), date_obs=as.Date(character()), heure_obs=as.Date(character()),temperature_eau_c=as.numeric())
@@ -33,3 +36,5 @@ for (file_name in names(data_list)) {
   # Add these to the date_observed data frame
   date_observed <- rbind(date_observed, selected_columns)
 }
+#put the data frame into a CSV file to be read into the data base
+write.csv(date_observed, "date_observed.csv",row.names=F)
