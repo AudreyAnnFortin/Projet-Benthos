6c6ebdbd080bc17cd086a1bd97e6225b65b2f558
marm1530 <marm1530@usherbrooke.ca>
2024-04-01 12:28:37 -0400

    j'ai ajouter des virgules dans les create table :)

--- Base de données.R
+++ Base de données.R
@@ -11,11 +11,11 @@ con <- dbConnect(SQLite(),dbname="benthos.db")
 # Creation of the table observation
 creer_observation<-
   "CREATE TABLE observations (
-      nom_sci   VARCHAR(100) PRIMARY KEY
-      site      VARCHAR(100)
-      date_obs  DATE
-      abondance REAL CHECK (abondance >= 0)
-      fraction  REAL CHECK (fraction >= 0)
+      nom_sci   VARCHAR(100) PRIMARY KEY,
+      site      VARCHAR(100),
+      date_obs  DATE,
+      abondance REAL CHECK (abondance >= 0),
+      fraction  REAL CHECK (fraction >= 0),
       FOREIGN KEY (site) REFERENCES site(site),
       FOREIGN KEY (date_obs) REFERENCES date_observed(date_obs)
   );"
@@ -25,10 +25,10 @@ dbWriteTable(con,append=TRUE,name="observations",value=observations,row.names=FA
 #Creation of the table site
 creer_site<-
   "CREATE TABLE sites (
-      site                VARCHAR(100) PRIMARY KEY
-      largeur_riviere     REAL
-      profondeur_riviere  REAL
-      vitesse_courant     REAL
+      site                VARCHAR(100) PRIMARY KEY,
+      largeur_riviere     REAL,
+      profondeur_riviere  REAL,
+      vitesse_courant     REAL,
       transparence_eau    REAL
 );"
 dbSendQuery(con, creer_site)
@@ -38,9 +38,9 @@ dbWriteTable(con,append=TRUE,name="sites",value=sites,row.names=FALSE)
 # Creation of the table date_observed
 creer_date_observed<-
   "CREATE TABLE date_observed (
-      date                VARCHAR(50)
-      heure_obs           VARCHAR(50)
-      date_obs            DATE
+      date                VARCHAR(50),
+      heure_obs           VARCHAR(50),
+      date_obs            DATE,
       temperature_eau_c   REAL
 );"
 dbSendQuery(con, creer_date_observed)
