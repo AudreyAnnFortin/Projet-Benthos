5382f20b4dd9344ff3c2443f0cc0631bb41bbd50
marm1530 <marm1530@usherbrooke.ca>
2024-04-23 11:55:45 -0400

    modification de la figure pour avoir le nb d'espèces observées par date d'observation

--- Scripts/figure_richesse.R
+++ Scripts/figure_richesse.R
@@ -6,27 +6,18 @@ library(RSQLite)
 con <- dbConnect(SQLite(), dbname = "benthos.db")
 
 # Charger les données de richesse spécifique depuis la base de données
-query_espece <- "SELECT nom_sci, site FROM observations"
+query_espece <- "SELECT date_obs, COUNT(DISTINCT nom_sci) AS nb_especes FROM observations GROUP BY date_obs"
 data_espece <- dbGetQuery(con, query_espece)
 
-# Charger les données des dates d'observations depuis la base de données
-query_date <- "SELECT date_obs, site FROM observations"
-data_date <- dbGetQuery(con, query_date)
-
-# Fusionner les données d'espèces avec les données de dates
-data_merged <- merge(data_espece, data_date, by = "site")
-
 # Convertir la colonne date_obs en format de date
-data_merged$date_obs <- as.Date(data_merged$date_obs)
-
-# Extraire le mois et l'année à partir de la date_obs
-data_merged$month_year <- format(data_merged$date_obs, "%b %Y")
+data_espece$date_obs <- as.Date(data_espece$date_obs)
 
 # Créer le graphique
-ggplot(data_merged, aes(x = month_year, fill = site)) +
-  geom_bar(position = "dodge") +
-  labs(title = "Nombre d'espèces observées dans le temps par site",
-       x = "Mois et année",
+ggplot(data_espece, aes(x = date_obs, y = nb_especes)) +
+  geom_point() + 
+  stat_smooth(method = "loess", se = FALSE) +  # Ajustement d'une courbe de régression
+  labs(title = "Nombre d'espèces observées à chaque date d'observation",
+       x = "Date d'observation",
        y = "Nombre d'espèces") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) # Rotation des étiquettes de l'axe x pour une meilleure lisibilité
