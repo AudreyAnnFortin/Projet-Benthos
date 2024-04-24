d58a8c6ffb65564137ebb1f638a72aa9c81e50e8
marm1530 <marm1530@usherbrooke.ca>
2024-04-24 13:04:34 -0400

    modifs afin davoir un data_list avec les donnees

--- Scripts/Data.R
+++ Scripts/Data.R
@@ -1,22 +1,29 @@
-
-Data <- function(directory_path = "./Données") {
-  # Define the function inside withr::with_dir()
-  withr::with_dir(directory_path, {
-    # Get a list of all CSV files in the directory
-    csv_files <- list.files(directory_path, pattern = "^site_.*\\.csv$", full.names = TRUE)
-    # Create an empty list to store data frames
-    data_list <- list()
-    
-    # Loop through each CSV file
-    for (file in csv_files) {
-      # Read data from CSV file
-      data <- read.csv(file)
-      # Extract file name without extension
-      file_name <- tools::file_path_sans_ext(basename(file))
-      # Store the data frame in the list with file name as key/index
-      data_list[[file_name]] <- data
-    }
-    # Return the populated data_list
-    return(data_list)
-  })
+Data <- function(subdirectory = "Données") {
+  # Save current working directory
+  original_dir <- getwd()
+  
+  # Change working directory to the specified subdirectory
+  setwd(file.path(original_dir, subdirectory))
+  
+  # Get a list of all CSV files in the directory
+  csv_files <- list.files(pattern = "^site_.*\\.csv$", full.names = TRUE)
+  # Create an empty list to store data frames
+  data_list <- list()
+  
+  # Loop through each CSV file
+  for (file in csv_files) {
+    # Read data from CSV file
+    data <- read.csv(file)
+    # Extract file name without extension
+    file_name <- tools::file_path_sans_ext(basename(file))
+    # Store the data frame in the list with file name as key/index
+    data_list[[file_name]] <- data
+  }
+  
+  # Restore original working directory
+  setwd(original_dir)
+  
+  # Return the populated data_list
+  return(data_list)
 }
+
