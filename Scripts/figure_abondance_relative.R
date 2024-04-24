7588a734cee9fdd14ab37e9cb58a282154f89aa6
marm1530 <marm1530@usherbrooke.ca>
2024-04-23 20:11:09 -0400

    raffinage

--- Scripts/figure_abondance_relative.R
+++ Scripts/figure_abondance_relative.R
@@ -1,13 +1,10 @@
-library(RSQLite)
-library(dplyr)
-library(ggplot2)
 
 # Connecter à la base de données
 con <- dbConnect(SQLite(), dbname = "benthos.db")
 
 # Définir la requête SQL pour sélectionner les 10 espèces les plus abondantes et regrouper les autres sous "Autres"
 query <- "
-  WITH Top10 AS (
+  WITH Top11 AS (
     SELECT 
       date_obs, 
       nom_sci,
@@ -19,13 +16,13 @@ query <- "
     GROUP BY 
       date_obs, nom_sci
     ORDER BY 
-      total_abondance DESC
-    LIMIT 10
+      total_abondance DESC, nom_sci DESC
+    LIMIT 17
   )
   SELECT 
     date_obs,
     CASE 
-      WHEN nom_sci IN (SELECT nom_sci FROM Top10) THEN nom_sci
+      WHEN nom_sci IN (SELECT nom_sci FROM Top11) THEN nom_sci
       ELSE 'Autres'
     END AS nom_sci,
     SUM(abondance) AS abondance
@@ -52,7 +49,7 @@ data_espece$nom_sci <- factor(data_espece$nom_sci, levels = esp_ordre)
 # Créer le graphique
 ggplot(data_espece, aes(x = format(date_obs, "%Y"), y = abondance, fill = nom_sci)) +
   geom_bar(stat = "identity") +
-  labs(title = "Abondance relative des espèces chaque année",
+  labs(title = "Abondance relative des espèces à chaque année",
        x = "Année d'observation",
        y = "Abondance relative des espèces",
        fill = "Espèce") +
