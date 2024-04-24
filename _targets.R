ca722b6ea2c3eaec9b9e69f8a3ed266b742aefa4
marm1530 <marm1530@usherbrooke.ca>
2024-04-23 21:16:03 -0400

    ceci est le vrai script _target

--- _targets.R
+++ _targets.R
@@ -1,23 +1,69 @@
+# ===========================================
+# _targets.R file
+# ===========================================
+# Dépendances
+library(dplyr)
+library(RSQLite)
+library(ggplot2)
 library(targets)
-# This is an example _targets.R file. Every
-# {targets} pipeline needs one.
-# Use tar_script() to create _targets.R and tar_edit()
-# to open it again for editing.
-# Then, run tar_make() to run the pipeline
-# and tar_read(data_summary) to view the results.
+library(tarchetypes) #Pour rendre le rapport (tar_render)
+tar_option_set(packages = c("rmarkdown", "knitr")) #Besoin de ces librairies afin d'obtenir le rapport final
 
-# Define custom functions and other global objects.
-# This is where you write source(\"R/functions.R\")
-# if you keep your functions in external scripts.
-summarize_data <- function(dataset) {
-  colMeans(dataset)
-}
+# Scripts R/fonctions nécessaires pour le target
+source("Scripts/Data.R")
+source("Scripts/Nettoyage.R")
+source("Scripts/tables.R")
+source("Scripts/Base de données.R")
+source("Scripts/figure_richesse.R")
+source("Scripts/figure_abondance_relative.R")
+source("Scripts/Bubble_plot_largeur.R")
+source("Scripts/Density_plot_profondeur.R")
 
-# Set target-specific options such as packages:
-# tar_option_set(packages = "utils") # nolint
-
-# End this file with a list of target objects.
+# Pipeline
 list(
-  tar_target(data, data.frame(x = sample.int(100), y = sample.int(100))),
-  tar_target(data_summary, summarize_data(data)) # Call your custom functions.
+  # Une target pour le chemin du fichier de donnée permet de suivre les 
+  # changements dans le fichier
+  tar_target(
+    name = path, # Cible
+    command = setwd("./Données"), # Emplacement du fichier
+  ), 
+  # La target suivante a "path" pour dépendance et importe les donnees. Sans
+  # la séparation de ces deux étapes, la dépendance serait brisée et une
+  # modification des données n'entrainerait pas l'exécution du pipeline
+  tar_target(
+    name = data, # Cible pour l'objet de données
+    command = Data(path) # Lecture des données
+  ),
+  tar_target(
+    name = cleaning, # Cible pour nettoyer et corriger les donnees
+    command = Nettoyage(data) # Nettoyage des données
+  ),
+  tar_target(
+    name = tables, # Cible pour la formation des tables
+    command = tables(cleaning) #Création des tables
+  ),
+  tar_target(
+    name = base_de_donnee, # Cible pour la formation d'une base de donnees
+    command = Base_de_donnee() #Formation de la base de donnees 
+  ),
+  tar_target(
+    name = figure_un , # Cible pour la création d'une première figure
+    command = richesse() #Création de la figure
+  ),
+  tar_target(
+    name = figure_deux , # Cible pour la création d'une deuxième figure
+    command = abondance() #Création de la figure
+  ),
+  tar_target(
+    name = figure_trois , # Cible pour la création d'une troisième figure
+    command = largeur() #Création de la figure
+  ),
+  tar_target(
+    name = figure_quatre , # Cible pour la création d'une troisième figure
+    command = profondeur() #Création de la figure
+  ),
+  tar_render( #Création du rapport
+    name = rapport , #Cible pour la création du rapport, le nom qui sera donné au document
+    path = "Rapport/rapport.Rmd" #Chemin vers le rapport
+  )
 )
