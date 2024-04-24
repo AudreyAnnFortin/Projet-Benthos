505cc0127f6d8dcf853bc2ce54446bbbbce8bd7e
marm1530 <marm1530@usherbrooke.ca>
2024-04-23 20:25:57 -0400

    modifier en bubble plot

--- Scripts/Density_plot_profondeur.R
+++ Scripts/Density_plot_profondeur.R
@@ -1,21 +1,34 @@
-# Charger les bibliothèques nécessaires
-library(RSQLite)
-library(ggplot2)
 
 # Connecter à la base de données
 con <- dbConnect(SQLite(), dbname = "benthos.db")
 
 query <- "
+  WITH Top10 AS (
+    SELECT sites.profondeur_riviere, observations.nom_sci, sum(observations.abondance) as abondance
+  FROM sites
+  INNER JOIN observations ON sites.site = observations.site
+  WHERE  sites.profondeur_riviere
+    GROUP BY 
+      observations.nom_sci
+    ORDER BY 
+      abondance DESC, observations.nom_sci DESC
+    LIMIT 15
+  )
   SELECT sites.profondeur_riviere, observations.nom_sci, observations.abondance
   FROM sites
   INNER JOIN observations ON sites.site = observations.site
+  WHERE  observations.nom_sci in (SELECT nom_sci FROM Top10)
+  GROUP BY observations.nom_sci, sites.profondeur_riviere
 "
 data <- dbGetQuery(con, query)
 
+#ggplot(data_merged, aes(x = largeur_riviere, y = abondance, size = abondance)) +
 # Créer le density plot avec ggplot2
-ggplot(data, aes(x = profondeur_riviere, fill = nom_sci)) +
-  geom_density(alpha = 0.5) +
-  labs(x = "Profondeur de la rivière", y = "Densité", fill = "Nom scientifique") +
-  theme_minimal()
+ggplot(data, aes(x = profondeur_riviere, y = nom_sci, size = abondance)) +#fill = nom_sci)) +
+  geom_point(alpha = 0.7) +
+  scale_size_continuous(range = c(3, 10)) +
+  labs(x = "Profondeur de la rivière", y = "Densité") +#, fill = "Nom scientifique") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))#+
+#theme_minimal()
 
 dbDisconnect(con)
